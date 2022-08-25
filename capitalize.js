a41de4bb09870d4e23b9d68c85dc6b394fc08557
BucurEva87 <bucur.eva87@gmail.com>
2022-08-25 18:47:51 +0300

    Add test case for argument not being a string to capitalize function

--- capitalize.js
+++ capitalize.js
@@ -1,1 +1,5 @@
-module.exports = (string) => `${string[0].toUpperCase()}${string.substr(1)}`;
+module.exports = (string) => {
+  if (typeof string !== 'string') throw new Error('Provided argument must be of type string');
+
+  return `${string[0].toUpperCase()}${string.substr(1)}`;
+};
--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -4,4 +4,7 @@ describe('capitalize', () => {
   test('returns the capitalized string', () => {
     expect(capitalize('microverse')).toBe('Microverse');
   });
+  test('throws an error if the argument is not of type string', () => {
+    expect(() => capitalize(24)).toThrow('Provided argument must be of type string');
+  });
 });
